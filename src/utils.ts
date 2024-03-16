4b2911d1c802e7c234d9af4d612dd30c0916e2e9
wxzhang <wxzhang@126.com>
2024-03-16 22:27:36 +0800

    update

--- src/utils.ts
+++ src/utils.ts
@@ -12,6 +12,11 @@ const badJsonRegex =/(\s*[\w\u4e00-\u9fa5]+\s*(?=:))|((?=:\s*)\'.*\')|(\'.*?\'(?
 // 匹配未添加逗号的行
 const addLineCommaRegex = /(?<!(\s*\,\s*)|([\[\{\}]\s*))\n(?!\s*\}\s*)/gm
 
+// 匹配使用""或''包裹字符串   
+const strVarRegex = /((?<!\\)\"|\')(.*?)((?<!\\)\1)/gm
+// 匹配任意没有使用""包裹的key
+const badKeyRegex = /([\s\[\,\{\b]{1})(?<!\"])(\w+)(?!\")(\s*\:)/gm
+
 /**
  * 解析非标的JSON字符串为{}
  * 非标的JSON字符串指的是：
@@ -23,27 +28,57 @@ const addLineCommaRegex = /(?<!(\s*\,\s*)|([\[\{\}]\s*))\n(?!\s*\}\s*)/gm
  */
 export function safeParseJson(str:string,callback?:(key:string,value:any)=>any){
     let matched;     
-    // 如果行未添加,则添加
-    let resultStr = str.replaceAll(addLineCommaRegex,",\n")
-                .replaceAll("，",",")
-                .replaceAll("“","\"")
-                .replaceAll("”","\"")
-    while ((matched = badJsonRegex.exec(resultStr)) !== null) {
-        if (matched.index === badJsonRegex.lastIndex) {
-            badJsonRegex.lastIndex++;
-        }                
-        let oldLen = resultStr.length
-        let item = matched[0].trim()
-        const matchedLength = matched[0].length
-        if(item.startsWith("'") && item.endsWith("'")){
-            item = item.substring(1,item.length-1)
-        }
-        item = '"'+item+'"'
-        resultStr = `${resultStr.substring(0,matched.index)}${item}${resultStr.substring(matched.index+matchedLength)}`
-        badJsonRegex.lastIndex += resultStr.length - oldLen
-    }
+    // 先尝试解析一个JSON字符串，如果解析失败，再尝试进行修复
     try{
-        return JSON.parse(resultStr,callback)
+        return JSON.parse(str,(key,value)=>{
+            if(callback){
+                return callback(key,value)
+            }
+            return value
+        })
+    }catch{}
+    
+
+    // 1. 如果行未添加逗号，添加逗号
+    let resultStr = str.replaceAll(addLineCommaRegex,",\n")
+    // 2. 匹配使用""或''包裹字符串，全部编码以防止后继正则出错
+    resultStr = resultStr.replaceAll(strVarRegex,(s,begin,value,end)=>{
+        return `\"${encodeURI(value)}\"`
+    })
+    // 3. 将key处理没有""包裹的全部加上""
+    resultStr = resultStr.replaceAll(badKeyRegex,(s,p1,value,p2)=>{
+        return `${p1}\"${value}\"${p2}`
+    })
+    // 4. 将一些全角字符转换为半角字符，容错
+    resultStr = resultStr.replaceAll("，",",")
+        .replaceAll("“","\"")
+        .replaceAll("”","\"")
+
+    
+    // // 将key处理没有""包裹的全部加上""
+
+    // while ((matched = badJsonRegex.exec(resultStr)) !== null) {
+    //     if (matched.index === badJsonRegex.lastIndex) {
+    //         badJsonRegex.lastIndex++;
+    //     }                
+    //     let oldLen = resultStr.length
+    //     let item = matched[0].trim()
+    //     const matchedLength = matched[0].length
+    //     if(item.startsWith("'") && item.endsWith("'")){
+    //         item = item.substring(1,item.length-1)
+    //     }
+    //     item = '"'+item+'"'
+    //     resultStr = `${resultStr.substring(0,matched.index)}${item}${resultStr.substring(matched.index+matchedLength)}`
+    //     badJsonRegex.lastIndex += resultStr.length - oldLen
+    // }
+    try{
+        return JSON.parse(resultStr,(key,value)=>{
+            if(typeof(value)=="string") value= decodeURI(value)
+            if(callback){
+                return callback(key,value)
+            }
+            return value
+        })
     }catch(e){
         return JSON.parse(str,callback)
     }
